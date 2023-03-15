82f39b484164908470c89e6258839fcb3e457795
Tenn456 <hiblubja@gmail.com>
2023-03-15 10:36:35 -0400

    Capstone

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer theSR;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    public KeyCode keyToPress;
+    public KeyCode keyToPress2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            theSR.sprite = pressedImage;
+        }
+
+        if (Input.GetKeyUp(keyToPress))
+        {
+            theSR.sprite = defaultImage;
+        }
+
+        if (Input.GetKeyDown(keyToPress2))
+        {
+            theSR.sprite = pressedImage;
+        }
+
+        if (Input.GetKeyUp(keyToPress2))
+        {
+            theSR.sprite = defaultImage;
+        }
+    }
+}
